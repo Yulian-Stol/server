fe438ab3da199719567e8eab19e53c900b3266e6
Yulian <kokomoko12345koko@gmail.com>
2021-09-22 16:51:32 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	//----------------------HAMBURGER-----------------------
+		const hamburger = (hamburgerButton, hamburgerNav, hamburgerHeader) => {
+			const button = document.querySelector(hamburgerButton),
+						nav = document.querySelector(hamburgerNav),
+						header = document.querySelector(hamburgerHeader);
+	
+			button.addEventListener('click', (e) => {
+				button.classList.toggle('hamburger--active');
+				nav.classList.toggle('header__nav--active');
+				header.classList.toggle('header--menu');
+			});
+	
+		};
+		hamburger('.hamburger', '.header__nav', '.header');
+
+});
